7d465bcb945f9cf71c0426aae24903197081c62d
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2025-06-16 22:03:55 +0700

    Login page sauce demo with combination invalid credentials

--- demo/src/test/java/seleniumWebAutomation/selenium_scenario/LoginWeb.java
+++ demo/src/test/java/seleniumWebAutomation/selenium_scenario/LoginWeb.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -48,6 +47,7 @@ public class LoginWeb {
         // You can add your test logic here
         System.out.println("Invalid credentials test is running.");
 
+        // kombinasi username password
         /*
          * 1. Valid Username , Invalid Password
          * 2. Invalid Username , Valid Password
