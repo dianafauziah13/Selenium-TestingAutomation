d199ea366f198fd5acc9375584dfadd1c5ea16dc
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2025-06-25 22:19:28 +0700

    Initial tugas project

---
+++ demo/src/test/java/seleniumWebAutomation/definitions/stepDefinitionsWeb.java
@@ -0,0 +1,15 @@
+package seleniumWebAutomation.definitions;
+
+import selenium.base.baseWebTest;
+
+public class stepDefinitionsWeb extends baseWebTest{
+    /*
+     *  Given User landing to logged ecommerce
+        When User input username "" and password ""
+        Then User redirect to homepage
+     */
+
+     /*
+      * TO BE CONTINUED COMMIT AND PUSH
+      */
+}
